cffc684640b6a31b46725128c0ab68d68d13e0ea
Leonardo Melo <leonardo.melo@gdstecnologia.com.br>
2026-01-27 22:24:11 -0300

    feat: initialize scraping model with environment setup and basic structure

--- scraping.py
+++ scraping.py
@@ -1,17 +1,17 @@
-import requests 
-from bs4 import BeautifulSoup
+# import requests 
+# from bs4 import BeautifulSoup
 
 
-pagina = requests.get("https://quotes.toscrape.com/")
+# pagina = requests.get("https://quotes.toscrape.com/")
 
-dados_pagina = BeautifulSoup(pagina.content, 'html.parser')
+# dados_pagina = BeautifulSoup(pagina.content, 'html.parser')
 
-# print(dados_pagina.prettify())
+# # print(dados_pagina.prettify())
 
-# def filtro(classe_css):
-#     return classe_css is not None and len(classe_css) == 4
+# # def filtro(classe_css):
+# #     return classe_css is not None and len(classe_css) == 4
 
-todas_frases = dados_pagina.find_all('span', itemprop="text")
+# todas_frases = dados_pagina.find_all('span', itemprop="text")
 
-for div in todas_frases:
-    print(div.text)
+# for div in todas_frases:
+#     print(div.text)
